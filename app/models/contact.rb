fb5d0fb6afda18f566fc1cead372635247c2c515
Ricardo Caldeira <ricardo.nezz@gmail.com>
2012-12-05 13:45:36 -0200

    All tests passing

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,10 @@
 class Contact < ActiveRecord::Base
   attr_accessible :email, :firstname, :lastname
+  validates :firstname, presence: true
+  validates :lastname, presence: true
+
+  def name
+  	"#{self.firstname} #{self.lastname}"
+  end
+
 end
--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -4,7 +4,13 @@ describe Contact do
 	it "has a valid factory" do
 		FactoryGirl.create(:contact).should be_valid
 	end
-	it "is invalid without a firstname"
-	it "is invalid without a lastname"
-	it "returns a contact's full name as a string"
+	it "is invalid without a firstname" do
+		FactoryGirl.build(:contact, firstname: nil).should_not be_valid
+	end
+	it "is invalid without a lastname" do
+		FactoryGirl.build(:contact, lastname: nil).should_not be_valid
+	end
+	it "returns a contact's full name as a string" do
+		FactoryGirl.build(:contact).name == "John Doe"
+	end
 end
