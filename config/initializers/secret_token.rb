03759a9c6221c5da91928ff3b8a79bb5ca4ede52
Ricardo Caldeira <ricardo.nezz@gmail.com>
2012-12-05 12:09:14 -0200

    Definição da estrutura inicial e das gems utilizadas

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Financeiro::Application.config.secret_token = 'f5a6f0b5aa6243fdc638c272555d508e6ddfee928dc0781499970c53ebdfcb2cc1aa4a4a75f0d548789428e0506373da78cc54336db598743b9c5abf5194933c'
